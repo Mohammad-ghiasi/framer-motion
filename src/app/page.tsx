f16990e01ad82f4c9a4c2c50d50f4549dd5af7bc
mohammad <mohammadghiasi005@gmail.com>
2024-06-30 07:03:57 +0330

    unmount-component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
 "use client"
-
-
 import Xmotion from "@/motions/Xmotion";
 import { Box, Button, Text } from "@chakra-ui/react";
-import { motion } from "framer-motion"
+import { AnimatePresence, motion } from "framer-motion"
 import { useState } from "react";
+
+const varinatMotion = {
+  hidden: {
+    x: '-100vh'
+  },
+  visible: {
+    x: '0',
+    transition: { type: 'spring', stiffness: 150 }
+  }
+}
 
 export default function Home() {
   const [show, setShow] = useState<boolean>(false)
@@ -32,6 +40,7 @@ export default function Home() {
         className="flex flex-col justify-center items-center"
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
+        transition={{ duration: 3 }}
       >
         <h2
           className=" font-bold mt-10"
@@ -49,16 +58,20 @@ export default function Home() {
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui, et amet ipsa aperiam minima nam omnis quasi facere ab neque placeat libero quos illum, mollitia debitis? Eius culpa modi repellendus.
         </Text>
         <Button colorScheme="blue" onClick={(): void => setShow(!show)}>click</Button>
-        {show && (
-          <Xmotion
-            className=" text-[20px] mt-10"
-            initial={{ x: '-100vh' }}
-            animate={{ x: '0' }}
-          >
-            Mohammad ghiasi is here :)
-          </Xmotion>
-        )}
-      </ motion.div>
+        <AnimatePresence>
+          {show && (
+            <Xmotion
+              className=" text-[20px] mt-10"
+              variants={varinatMotion}
+              initial='hidden'
+              animate='visible'
+              exit={{ opacity: 0 }}
+            >
+              Mohammad ghiasi is here :)
+            </Xmotion>
+          )}
+        </AnimatePresence>
+      </ motion.div >
     </>
   );
 }
